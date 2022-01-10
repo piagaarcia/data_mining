e00e439e0736b807325cff3e2512c434063674f8
Pia Garcia <piagarcia1999@gmail.com>
2022-01-10 18:11:11 +0100

    ajout des commentaires

--- projet.R
+++ projet.R
@@ -1,3 +1,72 @@
+"
+L'objectif de ce travail est de faire un analyse des données de l'app de rencontres OkCupid.
+Pour le faire, nous nous sommes servis d'une base de données disponible sur le site Kaggle (https://www.kaggle.com/andrewmvd/okcupid-profiles)
+
+Cette base contient 31 variables :
+
+
+
+age: age of user with random noise added for anonymization
+
+body_type- rather not say, thin, overweight, skinny, average, fit, athletic, jacked, a little extra, curvy, full figured, used up
+
+diet- mostly/strictly; anything, vegetarian, vegan, kosher, halal, other
+
+drinks- very often, often, socially, rarely, desperately, not at all
+
+drugs- never, sometimes, often
+
+education- graduated from, working on, dropped out of; high school, two-year college, university, masters program, law school, med school, Ph.D program, space camp
+
+ethnicity- Asian, middle eastern, black, native American, indian, pacific islander, Hispanic/latin, white, other
+
+height- inches
+
+income- (US $, -1 means rather not say) -1, 20000, 30000, 40000, 50000, 60000 70000, 80000, 100000, 150000, 250000, 500000, 1000000,
+
+job- student, art/music/writing, banking/finance, administration, technology, construction, education, entertainment/media, management, hospitality, law, medicine, military, politics/government, sales/marketing, science/engineering, transportation, unemployed, other, rather not say, retire
+
+offspring- has a kid, has kids, doesnt have a kid, doesn't want kids; ,and/,but might want them, wants them, doesnt want any, doesnt want more
+
+orientation- straight, gay, bisexual
+
+pets- has dogs, likes dogs, dislikes dogs; and has cats, likes cats, dislikes cats
+
+religion- agnosticism, atheism, Christianity, Judaism, Catholicism, Islam, Hinduism, Buddhism, Other; and very serious about it, and somewhat serious about it, but not too serious about it, and laughing about it
+
+sex- m, f
+
+sign- aquarius, pices, aries, Taurus, Gemini, cancer, leo, virgo, libra, scorpio, saggitarius, Capricorn; but it doesn’t matter, and it matters a lot, and it’s fun to think about
+
+smokes- yes, sometimes, when drinking, trying to quit, no
+
+speaks- English (fluently, okay, poorly). Afrikaans, Albanian, Arabic, Armenian, Basque, Belarusan, Bengali, Breton, Bulgarian, Catalan, Cebuano, Chechen, Chinese, C++, Croatian, Czech, Danish, Dutch, Esperanto, Estonian, Farsi, Finnish, French, Frisian, Georgian, German, Greek, Gujarati, Ancient Greek, Hawaiian, Hebrew, Hindi, Hungarian, Icelandic, Ilongo, Indonesian, Irish, Italian, Japanese, Khmer, Korean, Latin, Latvian, LISP, Lithuanian, Malay, Maori, Mongolian, Norwegian, Occitan, Other, Persian, Polish, Portuguese, Romanian, Rotuman, Russian, Sanskrit, Sardinian, Serbian, Sign Language, Slovak, Slovenian, Spanish, Swahili, Swedish, Tagalog, Tamil, Thai, Tibetan, Turkish, Ukranian, Urdu, Vietnamese, Welsh, Yiddish (fluently, okay, poorly)
+
+status- single, seeing someone, married, in an open relationship
+
+
+
+
+
+essay0- My self summary
+essay1- What I’m doing with my life
+essay2- I’m really good at
+essay3- The first thing people usually notice about me
+essay4- Favorite books, movies, show, music, and food
+essay5- The six things I could never do without
+essay6- I spend a lot of time thinking about
+essay7- On a typical Friday night I am
+essay8- The most private thing I am willing to admit
+essay9- You should message me if...
+
+
+
+"
+
+
+
+
+
 #librairies:
 
 library(dplyr)
@@ -8,8 +77,7 @@ library(dplyr)
 library(tidyverse)
 
 
-#Importer la base de données dans un dataframe nommée okcupid.
-
+#Importer la base de données dans un dataframe nommée data
 
 data<-read.csv("C:/Users/maria/OneDrive/Escritorio/PROJETS MoSEF 2021-2022/Data Mining/okcupid_profiles.csv", stringsAsFactors=FALSE)
 
@@ -18,19 +86,21 @@ data<-read.csv("C:/Users/maria/OneDrive/Escritorio/PROJETS MoSEF 2021-2022/Data 
 #Pour les variables categoriques on aura sa taille (length) class et mode.
 
 
+#les valeurs manquantes sont represnetes par des espaces, on veut que ce soit NA pour que R puisse comprendre
+
 data[data==""]<-NA
 
 
 str(data) #info
 
-colSums(is.na(data))  #count null values
+colSums(is.na(data))  #combien de valeurs nulles
 
  
-summary(data)   #summary
+summary(data)   #résumé des stats
 
 
 
-#diviser le dataset entre variables qualitatives en numeriques
+#diviser le dataset entre variables numeriques et qualitatives
 
 num<-data %>%
   dplyr::select(where(is.numeric))
@@ -55,9 +125,10 @@ for(x in seq(1,length(num),1)) boxplot(num[,x],col="pink",main=names(num[x]))
 #AGE: il y a des outliers au dessous du Q3. Il y a des valeurs extremement aberrants (MAX=110). fAUDRA L'Eliminer.
 #on peut dire que la pluplart des gens sont des jeunes, plus de 20 ans mais moins de 40.
 
-#HEIGHT: idk le format utilisee...
+#HEIGHT : height in inches--> taille en pouces
 
-#INCOME: problematique: la pluplart des valeurs sont -1: les gens ne publient pas leur income. ON PEUT L'ELIMINER.
+#INCOME: problematique: la plupart des valeurs sont -1: les gens ne publient pas leur income. ON PEUT L'ELIMINER.
+#US $, -1 means rather not say
 
 
 #drop income de la base des numeriques
@@ -184,7 +255,21 @@ colSums(is.na(quali2))
 
 #supervisé: predire qqchose
 
-#analyse des sentiments selon essays
+
+
+"analyse des sentiments selon essays
+
+essay0- My self summary
+essay1- What I’m doing with my life
+essay2- I’m really good at
+essay3- The first thing people usually notice about me
+essay4- Favorite books, movies, show, music, and food
+essay5- The six things I could never do without
+essay6- I spend a lot of time thinking about
+essay7- On a typical Friday night I am
+essay8- The most private thing I am willing to admit
+essay9- You should message me if...
+"  
 
 
 
